38071ff3b255c7dd36c8e9e97cb31e76109f4dd3
jgomezz <jgomezz@gmail.com>
2025-04-27 18:57:42 -0500

    Initial Commit

---
+++ apple_lab07_04_Basic_UICollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  apple_lab07_04_Basic_UICollectionView
+//
+//  Created by Jaime Gomez on 27/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
